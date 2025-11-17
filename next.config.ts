689996ca6e0f7e2a0932a4f45af6072f2e988b78
elaljo <elaljooo@gmail.com>
2025-11-17 20:19:42 +0100

    Fix vercel build: remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Webpack config for stable hot reload on Windows/WSL
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000, // check for changes every second
-        aggregateTimeout: 300,
-        ignored: ['**/node_modules/**', '**/.next/**', '**/.git/**'],
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
